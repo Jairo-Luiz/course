d57ff58ab876586a49323125c172fdc8f83d85e5
Jairo Luiz <jairols86@gmail.com>
2023-04-02 22:58:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entidades/User.java
+++ src/main/java/com/educandoweb/course/entidades/User.java
@@ -1,20 +1,40 @@
 package com.educandoweb.course.entidades;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import java.io.Serializable;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
+    @Override
+    public boolean equals ( Object o ) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass( this ) != Hibernate.getClass( o )) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals( getId(), user.getId() );
+    }
+
+    @Override
+    public int hashCode () {
+        return getClass().hashCode();
+    }
 }
